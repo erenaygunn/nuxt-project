440ecb155ad76960ad7ecd7e614ba8c3a6e5abda
erenaygunn <eaygun178@gmail.com>
2024-05-03 15:16:26 +0300

    chore: Add AgentCard component for displaying agent details

---
+++ components/Agents/AgentCard.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="col-md-4">
+      <div class="card-box-d">
+        <div class="card-img-d">
+          <img :src="agent.imageSrc" :alt="agent.imageAlt" class="img-d img-fluid">
+        </div>
+        <div class="card-overlay card-overlay-hover">
+          <div class="card-header-d">
+            <div class="card-title-d align-self-center">
+              <h3 class="title-d">
+                <nuxt-link :to="agent.link" class="link-two">{{ agent.name }}</nuxt-link>
+              </h3>
+            </div>
+          </div>
+          <div class="card-body-d">
+            <p class="content-d color-text-a">{{ agent.description }}</p>
+            <div class="info-agents color-a">
+              <p>
+                <strong>Phone: </strong> {{ agent.phone }}</p>
+              <p>
+                <strong>Email: </strong> {{ agent.email }}</p>
+            </div>
+          </div>
+          <div class="card-footer-d">
+            <div class="socials-footer d-flex justify-content-center">
+              <ul class="list-inline">
+                <li v-for="(social, i) in agent.socials" :key="i" class="list-inline-item">
+                  <a :href="social.link" class="link-one">
+                    <i :class="social.iconClass" aria-hidden="true"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      agent: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  </style>
--- components/Agents/Agents.vue
+++ components/Agents/Agents.vue
@@ -1,64 +1,33 @@
 <template>
     <section class="section-agents section-t8">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="title-wrap d-flex justify-content-between">
-              <div class="title-box">
-                <h2 class="title-a">Best Agents</h2>
-              </div>
-              <div class="title-link">
-                <nuxt-link to="/agents-grid">All Agents
-                  <span class="ion-ios-arrow-forward"></span>
-                </nuxt-link>
-              </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="title-wrap d-flex justify-content-between">
+            <div class="title-box">
+              <h2 class="title-a">Best Agents</h2>
             </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-4" v-for="(agent, index) in agents" :key="index">
-            <div class="card-box-d">
-              <div class="card-img-d">
-                <img :src="agent.imageSrc" :alt="agent.imageAlt" class="img-d img-fluid">
-              </div>
-              <div class="card-overlay card-overlay-hover">
-                <div class="card-header-d">
-                  <div class="card-title-d align-self-center">
-                    <h3 class="title-d">
-                      <nuxt-link :to="agent.link" class="link-two">{{ agent.name }}</nuxt-link>
-                    </h3>
-                  </div>
-                </div>
-                <div class="card-body-d">
-                  <p class="content-d color-text-a">{{ agent.description }}</p>
-                  <div class="info-agents color-a">
-                    <p>
-                      <strong>Phone: </strong> {{ agent.phone }}</p>
-                    <p>
-                      <strong>Email: </strong> {{ agent.email }}</p>
-                  </div>
-                </div>
-                <div class="card-footer-d">
-                  <div class="socials-footer d-flex justify-content-center">
-                    <ul class="list-inline">
-                      <li v-for="(social, i) in agent.socials" :key="i" class="list-inline-item">
-                        <a :href="social.link" class="link-one">
-                          <i :class="social.iconClass" aria-hidden="true"></i>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
+            <div class="title-link">
+              <nuxt-link to="/agents-grid">All Agents
+                <span class="ion-ios-arrow-forward"></span>
+              </nuxt-link>
             </div>
           </div>
         </div>
       </div>
-    </section>
-  </template>
+      <div class="row">
+        <AgentCard v-for="(agent, index) in agents" :key="index" :agent="agent" />
+      </div>
+    </div>
+  </section>
+</template>
   
-  <script>
-  export default {
+<script>
+import AgentCard from './AgentCard.vue';
+    export default {
+    components: {
+        AgentCard
+    },
     data() {
       return {
         agents: [
