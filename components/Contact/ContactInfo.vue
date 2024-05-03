cd9e17eee42e06b5e2aa7abc58bfb9ad00f7cc86
erenaygunn <eaygun178@gmail.com>
2024-05-03 22:46:53 +0300

    feat: Add ContactMap, ContactForm, and ContactInfo components for contact page

---
+++ components/Contact/ContactInfo.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="col-md-5 section-md-t3">
+              <div class="icon-box section-b2">
+                <div class="icon-box-icon">
+                  <span class="ion-ios-paper-plane"></span>
+                </div>
+                <div class="icon-box-content table-cell">
+                  <div class="icon-box-title">
+                    <h4 class="icon-title">Say Hello</h4>
+                  </div>
+                  <div class="icon-box-content">
+                    <p class="mb-1">Email.
+                      <span class="color-a">contact@example.com</span>
+                    </p>
+                    <p class="mb-1">Phone.
+                      <span class="color-a">+54 356 945234</span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="icon-box section-b2">
+                <div class="icon-box-icon">
+                  <span class="ion-ios-pin"></span>
+                </div>
+                <div class="icon-box-content table-cell">
+                  <div class="icon-box-title">
+                    <h4 class="icon-title">Find us in</h4>
+                  </div>
+                  <div class="icon-box-content">
+                    <p class="mb-1">
+                      Manhattan, Nueva York 10036,
+                      <br> EE. UU.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="icon-box">
+                <div class="icon-box-icon">
+                  <span class="ion-ios-redo"></span>
+                </div>
+                <div class="icon-box-content table-cell">
+                  <div class="icon-box-title">
+                    <h4 class="icon-title">Social networks</h4>
+                  </div>
+                  <div class="icon-box-content">
+                    <div class="socials-footer">
+                      <ul class="list-inline">
+                        <li class="list-inline-item">
+                          <a href="#" class="link-one">
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                          </a>
+                        </li>
+                        <li class="list-inline-item">
+                          <a href="#" class="link-one">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                          </a>
+                        </li>
+                        <li class="list-inline-item">
+                          <a href="#" class="link-one">
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                          </a>
+                        </li>
+                        <li class="list-inline-item">
+                          <a href="#" class="link-one">
+                            <i class="fa fa-pinterest-p" aria-hidden="true"></i>
+                          </a>
+                        </li>
+                        <li class="list-inline-item">
+                          <a href="#" class="link-one">
+                            <i class="fa fa-dribbble" aria-hidden="true"></i>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ContactInfo'
+  };
+  </script>
+  
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,31 @@
 <template>
     <IntroSingle title="Contact Us" breadcrumb="Contact" 
     text="Aut voluptas consequatur unde sed omnis ex placeat quis eos. Aut natus officia corrupti qui autem fugit consectetur quo. Et ipsum eveniet laboriosam voluptas beatae possimus qui ducimus. Et voluptatem deleniti. Voluptatum voluptatibus amet. Et esse sed omnis inventore hic culpa."/>
-    
+    <section class="contact">
+    <div class="container">
+      <div class="row">
+        <ContactMap />
+        <div class="col-sm12 section-t8">
+            <div class="row">
+                <ContactForm />
+                <ContactInfo />
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+import ContactMap from '../components/Contact/ContactMap.vue';
+import ContactForm from '../components/Contact/ContactForm.vue';
+import ContactInfo from '../components/Contact/ContactInfo.vue';
+
+export default {
+  components: {
+    ContactMap,
+    ContactForm,
+    ContactInfo
+  }
+};
+</script>
