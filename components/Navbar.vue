80adec230fe5d9435ae3717645ee0f949b53427c
erenaygunn <eaygun178@gmail.com>
2024-05-03 22:57:01 +0300

    refactor: Update Navbar.vue to use Nuxt links for improved navigation

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,7 +7,7 @@
           <span></span>
           <span></span>
         </button>
-        <a class="navbar-brand text-brand" href="index.html">Estate<span class="color-b">Agency</span></a>
+        <nuxt-link class="navbar-brand text-brand" href="/">Estate<span class="color-b">Agency</span></nuxt-link>
         <button type="button" class="btn btn-link nav-search navbar-toggle-box-collapse d-md-none" data-toggle="collapse"
           data-target="#navbarTogglerDemo01" aria-expanded="false">
           <span class="fa fa-search" aria-hidden="true"></span>
@@ -15,16 +15,16 @@
         <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <nuxt-link class="nav-link active" to="/">Home</nuxt-link>
+              <nuxt-link class="nav-link" active-class="active" to="/">Home</nuxt-link>
             </li>
             <li class="nav-item">
-              <nuxt-link class="nav-link" to="/about">About</nuxt-link>
+              <nuxt-link class="nav-link" active-class="active" to="/about">About</nuxt-link>
             </li>
             <li class="nav-item">
-              <nuxt-link class="nav-link" to="/property-grid">Property</nuxt-link>
+              <nuxt-link class="nav-link" active-class="active" to="/property-grid">Property</nuxt-link>
             </li>
             <li class="nav-item">
-              <nuxt-link class="nav-link" to="/blog-grid">Blog</nuxt-link>
+              <nuxt-link class="nav-link" active-class="active" to="/blog-grid">Blog</nuxt-link>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
