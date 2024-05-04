279821d53981d31c903d3621e99ceb840189a45f
erenaygunn <eaygun178@gmail.com>
2024-05-04 23:16:03 +0300

    refactor: Update import paths for JavaScript dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: [
-    'public/lib/bootstrap/css/bootstrap.min.css',
-    'public/lib/font-awesome/css/font-awesome.min.css',
-    'public/lib/animate/animate.min.css',
-    'public/lib/ionicons/css/ionicons.min.css',
-    'public/lib/owlcarousel/assets/owl.carousel.min.css',
-    '~/css/style.css'
-  ],
   app: {
     head: {
       script: [
@@ -20,6 +12,14 @@ export default defineNuxtConfig({
         { src: 'lib/owlcarousel/owl.carousel.min.js'},
         { src: 'lib/scrollreveal/scrollreveal.min.js'},
         { src: 'js/main.js' },
+      ],
+      link: [
+        { rel: 'stylesheet', href: 'lib/bootstrap/css/bootstrap.min.css' },
+        { rel: 'stylesheet', href: 'lib/font-awesome/css/font-awesome.min.css' },
+        { rel: 'stylesheet', href: 'lib/animate/animate.min.css' },
+        { rel: 'stylesheet', href: 'lib/ionicons/css/ionicons.min.css' },
+        { rel: 'stylesheet', href: 'lib/owlcarousel/assets/owl.carousel.min.css' },
+        { rel: 'stylesheet', href: 'css/style.css' },
       ],
     },
   },
