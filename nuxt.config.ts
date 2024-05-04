b82d7293fbeceadaf1e1bce4f05a73e92f860d63
erenaygunn <eaygun178@gmail.com>
2024-05-04 18:41:51 +0300

    refactor: Update import paths for JavaScript dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,24 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
-    '~/lib/bootstrap/css/bootstrap.min.css',
-    '~/lib/font-awesome/css/font-awesome.min.css',
-    '~/lib/animate/animate.min.css',
-    '~/lib/ionicons/css/ionicons.min.css',
-    '~/lib/owlcarousel/assets/owl.carousel.min.css',
+    'public/lib/bootstrap/css/bootstrap.min.css',
+    'public/lib/font-awesome/css/font-awesome.min.css',
+    'public/lib/animate/animate.min.css',
+    'public/lib/ionicons/css/ionicons.min.css',
+    'public/lib/owlcarousel/assets/owl.carousel.min.css',
     '~/css/style.css'
   ],
   app: {
     head: {
       script: [
-        { src: '~/lib/jquery/jquery.min.js'},
-        { src: '~/lib/jquery/jquery-migrate.min.js'},
-        { src: '~/lib/popper/popper.min.js'},
-        { src: '~/lib/bootstrap/js/bootstrap.min.js', type: 'module'},
-        { src: '~/lib/easing/easing.min.js' },
-        { src: '~/lib/owlcarousel/owl.carousel.min.js'},
-        { src: '~/lib/scrollreveal/scrollreveal.min.js'},
-        { src: '~/js/main.js' },
+        { src: 'lib/jquery/jquery.min.js'},
+        { src: 'lib/jquery/jquery-migrate.min.js'},
+        { src: 'lib/popper/popper.min.js'},
+        { src: 'lib/bootstrap/js/bootstrap.min.js'},
+        { src: 'lib/easing/easing.min.js' },
+        { src: 'lib/owlcarousel/owl.carousel.min.js'},
+        { src: 'lib/scrollreveal/scrollreveal.min.js'},
+        { src: 'js/main.js' },
       ],
     },
   },
